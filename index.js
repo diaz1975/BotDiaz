e22e8f02f44a40e8501874663a92294f41121f99
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-06-13 21:58:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('Katashi\nBot\nMD', {
+say('Diaz\nBot\nV1', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por KatashiFukushima`, {
+say(`Por DiazBot`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
