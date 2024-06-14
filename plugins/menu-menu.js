a6fb2eda2d40f7cec0c3576ca00fc46caaff61ee
DiazBot-MD <diazpatinoandresfelipe@gmail.com>
2024-06-13 22:03:09 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -149,17 +149,15 @@ let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.ge
 > *_${formatDate}_*
 > \`${nombreLugar} - ${ciudad}\`
 
-╭═〘卐 _Katashi bot MD_ 卐〙═⊷❍
+╭═〘卐 _Diaz bot v1_ 卐〙═⊷❍
 ┃☭╭───────────
-┃☭│ 〘 卐 _ᴏᴡɴᴇʀs ᴏғᴄ_ 卐 〙
+┃☭│ 〘 卐 _owner ofc_ 卐 〙
 ┃☭│ 
-┃☭│ _ᴋᴀᴛᴀsʜɪ ғᴜᴋᴜsʜɪᴍᴀ 🥷🏻_
-┃☭│ _ɪᴢɪ_ 𝅙
-┃☭│ シWɪʟsᴏɴシ︎
+┃☭│ _Díaz Mod  🥷🏻_
 ┃☭│
 ┃☭│    ║▌│█║▌│█║▌│█│║▌║
 ┃☭│    ║▌│█║▌│█║▌│█│║▌║
-┃☭│ᴋᴀᴛᴀsʜɪ ʙᴏᴛ x ᴋᴀᴛᴀsʜɪ ғᴜᴋᴜsʜɪᴍᴀ
+┃☭│Diaz Bot x Diaz Mod
 ┃☭│ 
 ┃☭╰────────────
 ╰══════════════⊷
@@ -167,7 +165,7 @@ let menu = `${lenguajeGB['smsConfi2']()} *${user.genero === 0 ? '👤' : user.ge
 卐     『  𝐌𝐄𝐍𝐔 𝐈𝐍𝐈𝐂𝐈𝐎  』    卐
 卐     ▼▼▼▼▼▼▼▼▼▼    卐
 
-╭〘卐 _INFO KATASHI BOT_ 卐〙⊷❍
+╭〘卐 _INFO DIAZ BOT_ 卐〙⊷❍
 ┃
 ┃☭│ ᩭ✎ *❰❰ ${lenguajeGB['smsTotalUsers']()} ❱❱* 
 ┃☭│ ᩭ✎ ➺ \`\`\`${Object.keys(global.db.data.users).length}\`\`\`
